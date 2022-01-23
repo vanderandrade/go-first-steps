309a9edefe4f8a9aee99d6f1f82389041fc0992e
vanderandrade <vander_c_andrade@hotmail.com>
2022-01-23 12:41:09 -0300

    Added constant message file

---
+++ app/src/messages/messages.go
@@ -0,0 +1,5 @@
+package messages
+
+const ERROR_CREATING_FILE = "Erro ao criar o arquivo"
+const ERROR_DOWNLOADING_FILE = "Erro ao baixar arquivo"
+const ERROR_SAVING_FILE = "Erro ao salvar o arquivo"
